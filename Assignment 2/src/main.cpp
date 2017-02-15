c484612db3dd8afe00243ac25fa77a0de9e7bccc
Diederik Vink <dav114@ic.ac.uk>
2017-02-15 18:05:34 +0000

    edited main

--- Assignment 2/src/main.cpp
+++ Assignment 2/src/main.cpp
@@ -6,6 +6,6 @@ int main() {
     Eigen::MatrixXd x1;
     Eigen::MatrixXd x2;
     generate_points (x1, x2, 100, 0, 2.5, -1, 2);
-    std::cout << "x1: \n" << x1 << std::endl;
-    std::cout << "x2: \n" << x2 << std::endl;
+    std::cout << "x1:\n" << x1 << std::endl;
+    std::cout << "x2:\n" << x2 << std::endl;
 }
